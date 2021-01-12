043f80dcddbbbab0d86619f0ccb0336cafa63705
guoxx03 <guoxx03@fenbi.com>
2021-01-12 17:18:00 +0800

    MOD: 改用fetch请求数据

--- react-cli/search-user/src/components/Search/index.jsx
+++ react-cli/search-user/src/components/Search/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Input, Button } from "antd";
-import axios from "axios";
+// import axios from "axios";
 import PubSub from "pubsub-js";
 
 import "./index.css";
@@ -20,9 +20,36 @@ export default class Search extends Component {
       isFirst: false,
       isLoading: true
     });
-    axios
-      .get(`https://api.github.com/search/users?q=${userName}`)
-      .then(({ data }) => {
+    // 使用axios
+    // axios
+    //   .get(`https://api.github.com/search/users?q=${userName}`)
+    //   .then(({ data }) => {
+    //     PubSub.publish(UPDATE_STATE, {
+    //       users: data.items,
+    //       isLoading: false
+    //     });
+    //   })
+    //   .catch(e => {
+    //     PubSub.publish(UPDATE_STATE, {
+    //       isLoading: false,
+    //       error: e.message
+    //     });
+    //   });
+
+    // 使用fetch
+    // 请求发送成功 但是服务器响应错误时 仍然会返回resolve的状态 此时需要通过 状态码来判断
+    fetch(`https://api.github.com/search/users1?q=${userName}`)
+      .then(data => {
+        // data 是一个 Response 对象
+        // 调用 Response 的 json 方法返回一个 Promise
+        if (data.ok) {
+          return data.json(data);
+        } else {
+          throw new Error(data.statusText);
+        }
+      })
+      .then(data => {
+        // 得到响应内容
         PubSub.publish(UPDATE_STATE, {
           users: data.items,
           isLoading: false
