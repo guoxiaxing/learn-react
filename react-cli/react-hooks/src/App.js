fc8ab861f6abb07fbd397a7578848d6ccc932cc8
guoxx03 <guoxx03@fenbi.com>
2021-01-29 09:40:40 +0800

    ADD: 路由组件懒加载

--- react-cli/react-hooks/src/App.js
+++ react-cli/react-hooks/src/App.js
@@ -1,10 +1,50 @@
 import "./App.css";
-import Demo from "./components/setState";
+// import Demo from "./components/setState";
+import "antd/dist/antd.css";
+import { Breadcrumb, Spin } from "antd";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Link
+  // withRouter
+} from "react-router-dom";
+// import Home from "./components/Home";
+// import About from "./components/About";
+import { lazy, Suspense } from "react";
+
+// 路由懒加载
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
 
 function App() {
   return (
+    // <div className="App">
+    //   <Demo name={"Demo setState"} />
+    // </div>
+
     <div className="App">
-      <Demo name={"Demo setState"} />
+      <Router>
+        <>
+          <h1>React Router Demo</h1>
+
+          <Breadcrumb>
+            <Breadcrumb.Item>
+              <Link to="/home">Home</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link to="/about">About</Link>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+          <Switch>
+            {/* fallback 当路由组件还没有异步加载到的时候 显示的组件（但是这个组件不能懒加载 需要使用直接 import 的形式来导入） */}
+            <Suspense fallback={<Spin />}>
+              <Route path="/home" component={Home} />
+              <Route path="/about" component={About} />
+            </Suspense>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
